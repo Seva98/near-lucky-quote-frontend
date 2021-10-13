f79a5bbe6ffcff0c94314d9db82ae89f91396cda
Ondrej Sevcik <ondrejsevcik@Ondrej-iMac.local>
2021-10-13 11:20:18 +0200

    v0.1

--- pages/index.js
+++ pages/index.js
@@ -1,89 +1,114 @@
+import Loader from '../components/common/loader';
+import Card from '../components/common/card';
 import Button from '../components/common/button';
 import Layout from '../components/common/layout';
 import Title from '../components/common/title';
 import Subtitle from '../components/common/subtitle';
-import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { keyStores, Near, WalletConnection, utils } from 'near-api-js';
-import BN from 'bn.js';
-import axios from 'axios';
-
-const CONTRACT_ID = 'sevcik.testnet';
-const gas = new BN('70000000000000');
+import { CONTRACT_ID, gas, getWallet, parseNear, getLuckyQuoteFromNEAR } from '../lib/near';
 
 export default function Home() {
-  //const accountId = wallet.getAccountId();
-  const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
+  const [wallet, setWallet] = useState();
   const [accountId, setAccountId] = useState('');
-  let near, wallet;
+  const [infoMessage, setInfoMessage] = useState([]);
+  const [luckyQuote, setLuckyQuote] = useState('');
 
   useEffect(() => {
-    near = new Near({
-      networkId: 'testnet',
-      keyStore: new keyStores.BrowserLocalStorageKeyStore(),
-      nodeUrl: 'https://rpc.testnet.near.org',
-      walletUrl: 'https://wallet.testnet.near.org',
-    });
-    wallet = new WalletConnection(near, 'sevcik');
-    console.log(wallet);
-    setAccountId(wallet.getAccountId());
+    setLoading(true);
+    try {
+      const tempWallet = getWallet();
+      setWallet(tempWallet);
+      setAccountId(tempWallet.getAccountId());
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
   }, []);
 
   const signIn = () => {
-    wallet.requestSignIn(CONTRACT_ID);
+    setLoading(true);
+    try {
+      wallet.requestSignIn(CONTRACT_ID);
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
   };
 
   const signOut = () => {
-    wallet.signOut();
-    router.replace(router.asPath);
+    setLoading(true);
+    try {
+      wallet.signOut();
+      localStorage.removeItem('near-api-js:keystore:sevcik.testnet:testnet');
+      setInfoMessage([]);
+      setLuckyQuote('');
+      setAccountId('');
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
   };
 
   const getLuckyQuote = async () => {
-    const result = await wallet.account().functionCall({
-      contractId: CONTRACT_ID,
-      methodName: 'generateLuckyQuote',
-      gas,
-      attachedDeposit: utils.format.parseNearAmount('3'),
-    });
-    const { hash } = result.transaction;
-    console.log(result);
-    console.log(hash);
+    setLoading(true);
+    const result = await getLuckyQuoteFromNEAR(wallet);
+    const {
+      status: { SuccessValue },
+    } = result;
+    console.log(contractResponse);
+    const contractResponse = atob(SuccessValue).split('\\n');
+    setLuckyQuote(formattedQuote(contractResponse));
+    setInfoMessage(formattedInfo(contractResponse));
+    setLoading(false);
   };
 
-  const getQuoteInfo = async () => {
-    const response = await axios.post('https://rpc.testnet.near.org', {
-      jsonrpc: '2.0',
-      id: 'dontcare',
-      method: 'tx',
-      params: ['J21opPxvz43qMxbGGm2bhUtoCTBFHXZ8S53q1uw2YpdD', accountId],
-    });
-    console.log(response);
-  };
+  const formattedInfo = (contractResponse) => contractResponse.slice(0, 2).join('\n').replaceAll(`"\"`, '').replaceAll(`"`, '').split('\n');
+  const formattedQuote = (contractResponse) => contractResponse[4].replaceAll('â', '').replaceAll('\u009c', '“').replaceAll('\u009d', '”').replaceAll('\u0093', '-');
 
   return (
     <Layout>
-      {accountId ? (
-        <>
-          <div>{accountId}</div>
-          <Button onClick={() => signOut()}>Sign Out</Button>
-        </>
-      ) : (
-        <Button onClick={() => signIn()}>Login with NEAR</Button>
-      )}
-
-      <Title level={1} className="ml-auto mr-auto">
-        Ing. Ondrej Sevcik
-      </Title>
-      <Subtitle>Overview of primary skills I have used over years</Subtitle>
-      <Button onClick={() => getLuckyQuote()}>Get lucky quote</Button>
-      <Button onClick={() => getQuoteInfo()}>Get quote info</Button>
+      <Loader loading={loading} />
+      <div className="text-center pt-20">
+        <Title level={1} className="mx-auto">
+          NEAR LUCKY QUOTE
+        </Title>
+        <Subtitle className="">Connect your NEAR testnet wallet and generate lucky quote about learning that is linked to your NEAR testnet account</Subtitle>
+        {accountId ? (
+          <>
+            <div className="mb-4">
+              Welcome{' '}
+              <a href={`https://explorer.testnet.near.org/accounts/${accountId}`} target="_blank">
+                {accountId}
+              </a>
+              , please click below to generate your Lucky Quote
+            </div>
+            {luckyQuote ? (
+              <Card>
+                <Title level={3} className="mx-auto">
+                  Your lucky quote
+                </Title>
+                <>
+                  <p className="font-subtitle text-xl">{luckyQuote}</p>
+                  {infoMessage.map((m) => (
+                    <p key={m} className="text-gray-300 text-sm">
+                      {m}
+                    </p>
+                  ))}
+                </>
+              </Card>
+            ) : (
+              <Button onClick={() => getLuckyQuote()}>Get lucky quote</Button>
+            )}
+            <div className="absolute bottom-1 w-full">
+              <Button onClick={() => signOut()}>Sign Out</Button>
+            </div>
+          </>
+        ) : (
+          <Button onClick={() => signIn()}>Login with NEAR</Button>
+        )}
+      </div>
     </Layout>
   );
-}
-
-export async function getStaticProps() {
-  return {
-    props: {},
-  };
 }
