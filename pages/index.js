7d578886d6e554c674949edbb6439f800b300b5e
Ondrej Sevcik <ondrejsevcik@Ondrej-iMac.local>
2021-10-13 12:15:24 +0200

    cleaning the code, added README

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Title from '../components/common/title';
 import Subtitle from '../components/common/subtitle';
 import { useEffect, useState } from 'react';
 import { CONTRACT_ID, gas, getWallet, parseNear, getLuckyQuoteFromNEAR } from '../lib/near';
+import Container from '../components/common/container';
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -71,44 +72,46 @@ export default function Home() {
 
   return (
     <Layout>
-      <Loader loading={loading} />
-      <div className="text-center pt-20">
-        <Title level={1} className="mx-auto">
-          NEAR LUCKY QUOTE
-        </Title>
-        <Subtitle className="">Connect your NEAR testnet wallet and generate lucky quote about learning that is linked to your NEAR testnet account</Subtitle>
-        {accountId ? (
-          <>
-            <div className="mb-4">
-              Welcome{' '}
-              <a href={`https://explorer.testnet.near.org/accounts/${accountId}`} target="_blank">
-                {accountId}
-              </a>
-              {!luckyQuote && <span>, please click below to generate your Lucky Quote</span>}
-            </div>
-            {luckyQuote ? (
-              <Card>
-                <Title level={3} className="mx-auto">
-                  Your lucky quote
-                </Title>
-                <>
-                  <p className="font-subtitle text-2xl">{luckyQuote}</p>
-                  {infoMessage.map((m) => (
-                    <p key={m} className="text-gray-300 text-md">
-                      {m}
-                    </p>
-                  ))}
-                </>
-              </Card>
-            ) : (
-              <Button onClick={() => getLuckyQuote()}>Get lucky quote</Button>
-            )}
-            <Button onClick={() => signOut()}>Sign Out</Button>
-          </>
-        ) : (
-          <Button onClick={() => signIn()}>Login with NEAR</Button>
-        )}
-      </div>
+      <Container>
+        <Loader loading={loading} />
+        <div className="text-center pt-20">
+          <Title level={1} className="mx-auto">
+            NEAR LUCKY QUOTE
+          </Title>
+          <Subtitle className="">Connect your NEAR testnet wallet and generate lucky quote about learning that is linked to your NEAR testnet account</Subtitle>
+          {accountId ? (
+            <>
+              <div className="mb-4">
+                Welcome{' '}
+                <a href={`https://explorer.testnet.near.org/accounts/${accountId}`} target="_blank">
+                  {accountId}
+                </a>
+                {!luckyQuote && <span>, please click below to generate your Lucky Quote</span>}
+              </div>
+              {luckyQuote ? (
+                <Card>
+                  <Title level={3} className="mx-auto">
+                    Your lucky quote
+                  </Title>
+                  <>
+                    <p className="font-subtitle text-2xl">{luckyQuote}</p>
+                    {infoMessage.map((m) => (
+                      <p key={m} className="text-gray-300 text-md">
+                        {m}
+                      </p>
+                    ))}
+                  </>
+                </Card>
+              ) : (
+                <Button onClick={() => getLuckyQuote()}>Get lucky quote</Button>
+              )}
+              <Button onClick={() => signOut()}>Sign Out</Button>
+            </>
+          ) : (
+            <Button onClick={() => signIn()}>Login with NEAR</Button>
+          )}
+        </div>
+      </Container>
     </Layout>
   );
 }
